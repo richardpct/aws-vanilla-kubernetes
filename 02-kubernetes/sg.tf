f651f6bdf8f7c70c64fe7d0fd394aba6732d4d4b
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2023-09-18 23:47:01 +0200

    groupping sg

--- 02-kubernetes/sg.tf
+++ 02-kubernetes/sg.tf
@@ -2,9 +2,39 @@ resource "aws_security_group" "kubernetes_master" {
   name   = "sg_kubernetes_master"
   vpc_id = data.terraform_remote_state.network.outputs.vpc_id
 
-  tags = {
-    Name = "Kubernetes master SG"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip_address]
   }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip_address]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Kubernetes master sg"
+  }
+}
+
+resource "aws_security_group_rule" "master_ingress_node" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.kubernetes_node.id
+  security_group_id        = aws_security_group.kubernetes_master.id
 }
 
 resource "aws_security_group" "kubernetes_node" {
@@ -12,7 +42,7 @@ resource "aws_security_group" "kubernetes_node" {
   vpc_id = data.terraform_remote_state.network.outputs.vpc_id
 
   tags = {
-    Name = "Kubernetes node SG"
+    Name = "Kubernetes node sg"
   }
 }
 
@@ -39,42 +69,6 @@ resource "aws_security_group" "lb_web" {
   }
 }
 
-resource "aws_security_group_rule" "master_inbound_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [var.my_ip_address]
-  security_group_id = aws_security_group.kubernetes_master.id
-}
-
-resource "aws_security_group_rule" "master_inbound_api" {
-  type              = "ingress"
-  from_port         = 6443
-  to_port           = 6443
-  protocol          = "tcp"
-  cidr_blocks       = [var.my_ip_address]
-  security_group_id = aws_security_group.kubernetes_master.id
-}
-
-resource "aws_security_group_rule" "master_inbound_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = [var.my_ip_address]
-  security_group_id = aws_security_group.kubernetes_master.id
-}
-
-resource "aws_security_group_rule" "master_outbound_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.kubernetes_master.id
-}
-
 resource "aws_security_group_rule" "node_outbound_all" {
   type              = "egress"
   from_port         = 0
@@ -91,15 +85,6 @@ resource "aws_security_group_rule" "master_to_node" {
   protocol                 = "-1"
   source_security_group_id = aws_security_group.kubernetes_master.id
   security_group_id        = aws_security_group.kubernetes_node.id
-}
-
-resource "aws_security_group_rule" "node_to_master" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.kubernetes_node.id
-  security_group_id        = aws_security_group.kubernetes_master.id
 }
 
 resource "aws_security_group_rule" "node_to_node" {
