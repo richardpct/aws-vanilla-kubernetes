b07fee8b68b924020b04c8f93280db0866bc3e2f
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2024-03-22 22:53:08 +0100

    using 3 AZ

--- 01-network/vars.tf
+++ 01-network/vars.tf
@@ -19,17 +19,17 @@ variable "subnet_public" {
 variable "subnet_public_lb" {
   type        = list(string)
   description = "Public lb subnet"
-  default     = ["192.168.1.0/24", "192.168.2.0/24"]
+  default     = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
 }
 
 variable "subnet_public_nat" {
   type        = list(string)
   description = "Public NAT subnet"
-  default     = ["192.168.3.0/24", "192.168.4.0/24"]
+  default     = ["192.168.4.0/24", "192.168.5.0/24", "192.168.6.0/24"]
 }
 
 variable "subnet_private_worker" {
   type        = list(string)
   description = "Subnet private worker"
-  default     = ["192.168.5.0/24", "192.168.6.0/24"]
+  default     = ["192.168.7.0/24", "192.168.8.0/24", "192.168.9.0/24"]
 }
