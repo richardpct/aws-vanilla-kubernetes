096c88e50bfb41c60308e4f924f963ab19c71ba5
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2026-05-08 20:40:17 +0200

    clean up security group

--- modules/network/sg.tf
+++ modules/network/sg.tf
@@ -1,103 +1,41 @@
+# bastion
 resource "aws_security_group" "bastion" {
   name   = "sg_bastion"
   vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = local.ssh_port
-    to_port     = local.ssh_port
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  egress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = "tcp"
-    cidr_blocks = local.anywhere
-  }
-
-  egress {
-    from_port   = local.https_port
-    to_port     = local.https_port
-    protocol    = "tcp"
-    cidr_blocks = local.anywhere
-  }
 
   tags = {
     Name = "bastion"
   }
 }
 
-resource "aws_security_group" "kubernetes_master" {
-  name   = "sg_kubernetes_master"
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = local.ssh_port
-    to_port     = local.ssh_port
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  ingress {
-    from_port   = local.kube_api_port
-    to_port     = local.kube_api_port
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  ingress {
-    from_port   = local.hubble_port
-    to_port     = local.hubble_port
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  ingress {
-    from_port   = local.kube_api_port
-    to_port     = local.kube_api_port
-    protocol    = "tcp"
-    cidr_blocks = [for nat_ip in aws_eip.nat[*].public_ip: "${nat_ip}/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = local.anywhere
-  }
-
-  tags = {
-    Name = "Kubernetes master sg"
-  }
+resource "aws_security_group_rule" "bastion_from_allow_ssh" {
+  type              = "ingress"
+  from_port         = local.ssh_port
+  to_port           = local.ssh_port
+  protocol          = "tcp"
+  cidr_blocks       = [var.my_ip_address]
+  security_group_id = aws_security_group.bastion.id
 }
 
-resource "aws_security_group" "efs" {
-  name   = "sg_efs"
-  vpc_id = aws_vpc.my_vpc.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = local.anywhere
-  }
-
-  tags = {
-    Name = "efs_sg"
-  }
+resource "aws_security_group_rule" "bastion_to_any_http" {
+  type              = "egress"
+  from_port         = local.http_port
+  to_port           = local.http_port
+  protocol          = "tcp"
+  cidr_blocks       = local.anywhere
+  security_group_id = aws_security_group.bastion.id
 }
 
-resource "aws_security_group_rule" "master_ingress_bastion" {
-  type                     = "ingress"
-  from_port                = local.ssh_port
-  to_port                  = local.ssh_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.bastion.id
-  security_group_id        = aws_security_group.kubernetes_master.id
+resource "aws_security_group_rule" "bastion_to_any_https" {
+  type              = "egress"
+  from_port         = local.https_port
+  to_port           = local.https_port
+  protocol          = "tcp"
+  cidr_blocks       = local.anywhere
+  security_group_id = aws_security_group.bastion.id
 }
 
-resource "aws_security_group_rule" "bastion_egress_master" {
+resource "aws_security_group_rule" "bastion_to_master_ssh" {
   type                     = "egress"
   from_port                = local.ssh_port
   to_port                  = local.ssh_port
@@ -106,16 +44,7 @@ resource "aws_security_group_rule" "bastion_egress_master" {
   security_group_id        = aws_security_group.bastion.id
 }
 
-resource "aws_security_group_rule" "worker_ingress_bastion" {
-  type                     = "ingress"
-  from_port                = local.ssh_port
-  to_port                  = local.ssh_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.bastion.id
-  security_group_id        = aws_security_group.kubernetes_worker.id
-}
-
-resource "aws_security_group_rule" "bastion_egress_worker" {
+resource "aws_security_group_rule" "bastion_to_worker_ssh" {
   type                     = "egress"
   from_port                = local.ssh_port
   to_port                  = local.ssh_port
@@ -124,113 +53,7 @@ resource "aws_security_group_rule" "bastion_egress_worker" {
   security_group_id        = aws_security_group.bastion.id
 }
 
-resource "aws_security_group_rule" "master_ingress_worker" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.kubernetes_worker.id
-  security_group_id        = aws_security_group.kubernetes_master.id
-}
-
-resource "aws_security_group" "kubernetes_worker" {
-  name   = "sg_kubernetes_worker"
-  vpc_id = aws_vpc.my_vpc.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = local.anywhere
-  }
-
-  tags = {
-    Name = "Kubernetes worker sg"
-  }
-}
-
-resource "aws_security_group_rule" "master_to_worker" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.kubernetes_master.id
-  security_group_id        = aws_security_group.kubernetes_worker.id
-}
-
-resource "aws_security_group_rule" "master_to_master" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.kubernetes_master.id
-  security_group_id        = aws_security_group.kubernetes_master.id
-}
-
-resource "aws_security_group_rule" "worker_to_worker" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.kubernetes_worker.id
-  security_group_id        = aws_security_group.kubernetes_worker.id
-}
-
-resource "aws_security_group_rule" "master_from_lb_external" {
-  type                     = "ingress"
-  from_port                = local.kube_api_port
-  to_port                  = local.kube_api_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.lb_external.id
-  security_group_id        = aws_security_group.kubernetes_master.id
-}
-
-resource "aws_security_group_rule" "master_from_lb_api_internal" {
-  type                     = "ingress"
-  from_port                = local.kube_api_port
-  to_port                  = local.kube_api_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.lb_api_internal.id
-  security_group_id        = aws_security_group.kubernetes_master.id
-}
-
-resource "aws_security_group_rule" "lb_api_internal_from_master" {
-  type                     = "ingress"
-  from_port                = local.kube_api_port
-  to_port                  = local.kube_api_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.kubernetes_master.id
-  security_group_id        = aws_security_group.lb_api_internal.id
-}
-
-resource "aws_security_group_rule" "worker_from_lb_api_internal" {
-  type                     = "ingress"
-  from_port                = local.kube_api_port
-  to_port                  = local.kube_api_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.lb_api_internal.id
-  security_group_id        = aws_security_group.kubernetes_worker.id
-}
-
-resource "aws_security_group_rule" "lb_api_internal_from_worker" {
-  type                     = "ingress"
-  from_port                = local.kube_api_port
-  to_port                  = local.kube_api_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.kubernetes_worker.id
-  security_group_id        = aws_security_group.lb_api_internal.id
-}
-
-resource "aws_security_group_rule" "worker_from_lb_https" {
-  type                     = "ingress"
-  from_port                = local.nodeport_https
-  to_port                  = local.nodeport_https
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.lb_external.id
-  security_group_id        = aws_security_group.kubernetes_worker.id
-}
-
-resource "aws_security_group_rule" "bastion_to_efs" {
+resource "aws_security_group_rule" "bastion_to_efs_nfs" {
   type                     = "egress"
   from_port                = local.nfs_port
   to_port                  = local.nfs_port
@@ -239,7 +62,145 @@ resource "aws_security_group_rule" "bastion_to_efs" {
   security_group_id        = aws_security_group.bastion.id
 }
 
-resource "aws_security_group_rule" "efs_from_bastion" {
+# kubernetes master
+resource "aws_security_group" "kubernetes_master" {
+  name   = "sg_kubernetes_master"
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "Kubernetes master sg"
+  }
+}
+
+resource "aws_security_group_rule" "master_from_bastion_ssh" {
+  type                     = "ingress"
+  from_port                = local.ssh_port
+  to_port                  = local.ssh_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion.id
+  security_group_id        = aws_security_group.kubernetes_master.id
+}
+
+resource "aws_security_group_rule" "master_from_worker_all" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.kubernetes_worker.id
+  security_group_id        = aws_security_group.kubernetes_master.id
+}
+
+resource "aws_security_group_rule" "master_from_master_all" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.kubernetes_master.id
+  security_group_id        = aws_security_group.kubernetes_master.id
+}
+
+resource "aws_security_group_rule" "master_from_lb_external_api" {
+  type                     = "ingress"
+  from_port                = local.kube_api_port
+  to_port                  = local.kube_api_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lb_external.id
+  security_group_id        = aws_security_group.kubernetes_master.id
+}
+
+resource "aws_security_group_rule" "master_from_lb_api_internal_api" {
+  type                     = "ingress"
+  from_port                = local.kube_api_port
+  to_port                  = local.kube_api_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lb_api_internal.id
+  security_group_id        = aws_security_group.kubernetes_master.id
+}
+
+resource "aws_security_group_rule" "master_to_any_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = local.anywhere
+  security_group_id = aws_security_group.kubernetes_master.id
+}
+
+# kubernetes worker
+resource "aws_security_group" "kubernetes_worker" {
+  name   = "sg_kubernetes_worker"
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "Kubernetes worker sg"
+  }
+}
+
+resource "aws_security_group_rule" "worker_from_bastion_ssh" {
+  type                     = "ingress"
+  from_port                = local.ssh_port
+  to_port                  = local.ssh_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion.id
+  security_group_id        = aws_security_group.kubernetes_worker.id
+}
+
+resource "aws_security_group_rule" "worker_from_master" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.kubernetes_master.id
+  security_group_id        = aws_security_group.kubernetes_worker.id
+}
+
+resource "aws_security_group_rule" "worker_from_worker" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.kubernetes_worker.id
+  security_group_id        = aws_security_group.kubernetes_worker.id
+}
+
+resource "aws_security_group_rule" "worker_from_lb_api_internal_api" {
+  type                     = "ingress"
+  from_port                = local.kube_api_port
+  to_port                  = local.kube_api_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lb_api_internal.id
+  security_group_id        = aws_security_group.kubernetes_worker.id
+}
+
+resource "aws_security_group_rule" "worker_from_lb_external_nodeport" {
+  type                     = "ingress"
+  from_port                = local.nodeport_https
+  to_port                  = local.nodeport_https
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lb_external.id
+  security_group_id        = aws_security_group.kubernetes_worker.id
+}
+
+resource "aws_security_group_rule" "worker_to_any_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = local.anywhere
+  security_group_id = aws_security_group.kubernetes_worker.id
+}
+
+# efs
+resource "aws_security_group" "efs" {
+  name   = "sg_efs"
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "efs_sg"
+  }
+}
+
+resource "aws_security_group_rule" "efs_from_bastion_nfs" {
   type                     = "ingress"
   from_port                = local.nfs_port
   to_port                  = local.nfs_port
@@ -248,7 +209,7 @@ resource "aws_security_group_rule" "efs_from_bastion" {
   security_group_id        = aws_security_group.efs.id
 }
 
-resource "aws_security_group_rule" "efs_from_master" {
+resource "aws_security_group_rule" "efs_from_master_nfs" {
   type                     = "ingress"
   from_port                = local.nfs_port
   to_port                  = local.nfs_port
@@ -257,7 +218,7 @@ resource "aws_security_group_rule" "efs_from_master" {
   security_group_id        = aws_security_group.efs.id
 }
 
-resource "aws_security_group_rule" "efs_from_worker" {
+resource "aws_security_group_rule" "efs_from_worker_nfs" {
   type                     = "ingress"
   from_port                = local.nfs_port
   to_port                  = local.nfs_port
@@ -266,65 +227,76 @@ resource "aws_security_group_rule" "efs_from_worker" {
   security_group_id        = aws_security_group.efs.id
 }
 
-resource "aws_security_group_rule" "bastion_from_worker" {
-  type                     = "ingress"
-  from_port                = 9100
-  to_port                  = 9100
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.kubernetes_worker.id
-  security_group_id        = aws_security_group.bastion.id
-}
-
-
+# lb external
 resource "aws_security_group" "lb_external" {
   name   = "sg_lb_external"
   vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = local.kube_api_port
-    to_port     = local.kube_api_port
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  ingress {
-    from_port   = local.https_port
-    to_port     = local.https_port
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  ingress {
-    from_port   = local.https_port
-    to_port     = local.https_port
-    protocol    = "tcp"
-    cidr_blocks = [for nat_ip in aws_eip.nat[*].public_ip : "${nat_ip}/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = local.anywhere
-  }
 
   tags = {
     Name = "lb_external_sg"
   }
 }
 
+resource "aws_security_group_rule" "lb_external_from_allow_api" {
+  type              = "ingress"
+  from_port         = local.kube_api_port
+  to_port           = local.kube_api_port
+  protocol          = "tcp"
+  cidr_blocks       = [var.my_ip_address]
+  security_group_id = aws_security_group.lb_external.id
+}
+
+resource "aws_security_group_rule" "lb_external_from_allow_https" {
+  type              = "ingress"
+  from_port         = local.https_port
+  to_port           = local.https_port
+  protocol          = "tcp"
+  cidr_blocks       = [var.my_ip_address]
+  security_group_id = aws_security_group.lb_external.id
+}
+
+resource "aws_security_group_rule" "lb_external_to_any_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = local.anywhere
+  security_group_id = aws_security_group.lb_external.id
+}
+
+# lb api internal
 resource "aws_security_group" "lb_api_internal" {
   name   = "sg_lb_api_internal"
   vpc_id = aws_vpc.my_vpc.id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = local.anywhere
-  }
-
   tags = {
     Name = "lb_api_sg_internal"
   }
+}
+
+resource "aws_security_group_rule" "lb_api_internal_from_master_api" {
+  type                     = "ingress"
+  from_port                = local.kube_api_port
+  to_port                  = local.kube_api_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.kubernetes_master.id
+  security_group_id        = aws_security_group.lb_api_internal.id
+}
+
+resource "aws_security_group_rule" "lb_api_internal_from_worker_api" {
+  type                     = "ingress"
+  from_port                = local.kube_api_port
+  to_port                  = local.kube_api_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.kubernetes_worker.id
+  security_group_id        = aws_security_group.lb_api_internal.id
+}
+
+resource "aws_security_group_rule" "lb_internal_to_any_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = local.anywhere
+  security_group_id = aws_security_group.lb_api_internal.id
 }
